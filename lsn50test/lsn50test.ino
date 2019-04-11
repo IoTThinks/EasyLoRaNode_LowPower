5c2c1aeef6da4f0796e5c17ee2770c047da8291c
IoTThinks.com <vinhdat82@gmail.com>
2019-04-11 12:04:50 +0700

    To test pin mapping

---
+++ lsn50test/lsn50test.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(PA10, OUTPUT);
+}
+
+void loop() {
+  Serial.println("Hello LSN50");
+  digitalWrite(PA10, HIGH);
+  Serial.println("On");
+  delay(2000);
+  digitalWrite(PA10, LOW);
+  Serial.println("Off");
+  delay(2000);
+}
